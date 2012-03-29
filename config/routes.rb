cd95fc2072fc2b314fa3ed8273bc2b4d0291482f
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-03-29 15:39:54 -0600

    adjusted route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.apples  "student_reports",  :controller => "student_reports"
-  map.oranges "lead_reports", :controller => "lead_reports"
+  map.student_reports  "student_reports",  :controller => "student_reports"
+  map.lead_reports "lead_reports", :controller => "lead_reports"
 
   map.namespace :admin do |admin|
     #map.bananas "bananas", :controller => "bananas"
